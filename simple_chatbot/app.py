c31ef1ffda511b4b0feafdcdb72eaf6a3dbb0118
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-04-12 22:05:02 +0200

    Added multimodality <3

--- simple_chatbot/app.py
+++ simple_chatbot/app.py
@@ -1,12 +1,27 @@
 """Main file, the entry point."""
 
-from typing import Any, Literal
+from typing import Any, Literal, cast, List, Dict, Union
 from chain import create_chat_chain as get_chain
+import base64
 
 import chainlit as cl
 from chainlit.input_widget import Select, TextInput
 from langchain_core.messages import MessageLikeRepresentation
 from langchain.schema import AIMessage, HumanMessage
+
+
+def image_to_base64(image_path: str) -> str:
+    """
+    Convert an image file to a base64 encoded string.
+
+    Args:
+    - image_path (str): The path to the image file.
+
+    Returns:
+    - str: The base64 encoded string of the image.
+    """
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
 
 @cl.on_chat_start
@@ -39,7 +54,30 @@ async def main(message: cl.Message):
     history: list[MessageLikeRepresentation] = cl.user_session.get("chat_history")  # type: ignore
     response = cl.Message(content="")
 
-    history.append(HumanMessage(content=message.content))
+    images = [file for file in message.elements if file.mime and "image" in file.mime]
+
+    if images and model not in ["gpt-4-turbo"]:
+        response.content = (
+            "Cannot process images! Please choose a model which supports images."
+        )
+        response.author = "System"
+        await response.send()
+        return None
+
+    content = [{"type": "text", "text": message.content}] + [
+        {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{image_to_base64(file.path)}"
+            },
+        }
+        for file in images
+        if file.path
+    ]
+
+    content = cast(List[Union[str, Dict]], content)
+
+    history.append(HumanMessage(content=content))
 
     await response.send()
 
